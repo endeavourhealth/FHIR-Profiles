7feb180b1bd2704e216435cc7e8695d021fdd143
jonnyry <jonny.rylands@vitalsignssoftware.com>
2016-03-29 20:07:09 +0100

    Remove FHIR tools

--- Tools/FhirProfilePublisher/FhirProfilePublisher.Engine/WebContent/Scripts/treeviewhelpers.js
+++
@@ -1,9 +0,0 @@
-﻿$(':checkbox').change(function ()
-{
-    var removedRows = $('.removed-row');
-
-    if ($(this).is(':checked'))
-        removedRows.hide();
-    else
-        removedRows.fadeIn('slow');
-});
